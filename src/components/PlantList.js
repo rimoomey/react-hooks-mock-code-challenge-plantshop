974528605d404c2ee194da423b05ce0d565d2a96
Rimon Moomey <rimon.moomey@gmail.com>
2022-11-30 09:24:00 -0600

    complete first deliverable

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PlantCard from "./PlantCard";
 
 function PlantList() {
-  return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
-  );
+  const [plants, setPlants] = useState([]);
+  const api = "http://localhost:6001/plants";
+
+  const fetchPlants = (link) => {
+    fetch(link)
+      .then((res) => res.json())
+      .then((data) => setPlants(data));
+  };
+
+  useEffect(() => {
+    fetchPlants(api);
+  }, []);
+
+  const makePlantCards = () => {
+    return plants.map((plant) => {
+      return <PlantCard key={plant.id} props={plant} />;
+    });
+  };
+
+  return <ul className="cards">{makePlantCards()}</ul>;
 }
 
 export default PlantList;
